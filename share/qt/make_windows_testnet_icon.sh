3f58a6adb9e1ffbb5f06cf9968aa2f17ef67e96e
Erik Mossberg <lingonvecka@gmail.com>
2014-04-13 01:24:47 +0200

    Fixed some image and icon paths

--- share/qt/make_windows_testnet_icon.sh
+++ share/qt/make_windows_testnet_icon.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # create multiresolution windows icon
-ICON_SRC=../../src/qt/res/icons/bitcoin_testnet.png
-ICON_DST=../../src/qt/res/icons/bitcoin_testnet.ico
-convert ${ICON_SRC} -resize 16x16 bitcoin_testnet-16.png
-convert ${ICON_SRC} -resize 32x32 bitcoin_testnet-32.png
-convert ${ICON_SRC} -resize 48x48 bitcoin_testnet-48.png
-convert bitcoin_testnet-16.png bitcoin_testnet-32.png bitcoin_testnet-48.png ${ICON_DST}
+ICON_SRC=../../src/qt/res/icons/testnet.png
+ICON_DST=../../src/qt/res/icons/testnet.ico
+convert ${ICON_SRC} -resize 16x16 testnet-16.png
+convert ${ICON_SRC} -resize 32x32 testnet-32.png
+convert ${ICON_SRC} -resize 48x48 testnet-48.png
+convert testnet-16.png testnet-32.png testnet-48.png ${ICON_DST}
 
