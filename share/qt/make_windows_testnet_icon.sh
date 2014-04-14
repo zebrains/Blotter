d12bbd5b1fb4abfed1b429ba4410749b1c602297
Paul Bradley <paul@paul-bradley.com>
2014-04-14 13:55:55 +0100

    Icon gen scripts

--- share/qt/make_windows_testnet_icon.sh
+++ share/qt/make_windows_testnet_icon.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # create multiresolution windows icon
-ICON_SRC=../../src/qt/res/icons/testnet.png
-ICON_DST=../../src/qt/res/icons/testnet.ico
-convert ${ICON_SRC} -resize 16x16 testnet-16.png
-convert ${ICON_SRC} -resize 32x32 testnet-32.png
-convert ${ICON_SRC} -resize 48x48 testnet-48.png
-convert testnet-16.png testnet-32.png testnet-48.png ${ICON_DST}
+ICON_SRC=../../src/qt/res/icons/monocle_testnet.png
+ICON_DST=../../src/qt/res/icons/monocle_testnet.ico
+convert ${ICON_SRC} -resize 16x16 monocle_testnet-16.png
+convert ${ICON_SRC} -resize 32x32 monocle_testnet-32.png
+convert ${ICON_SRC} -resize 48x48 monocle_testnet-48.png
+convert monocle_testnet-16.png monocle_testnet-32.png monocle_testnet-48.png ${ICON_DST}
 
