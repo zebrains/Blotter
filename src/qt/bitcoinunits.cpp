dccb7719773ff13fa17648350b37cc722d7a14ca
Erik Mossberg <lingonvecka@gmail.com>
2014-04-14 15:56:15 +0200

    updated max coins

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -67,9 +67,9 @@ int BitcoinUnits::amountDigits(int unit)
 {
     switch(unit)
     {
-    case BTC: return 8; // 21,000,000 (# digits, without commas)
-    case mBTC: return 11; // 21,000,000,000
-    case uBTC: return 14; // 21,000,000,000,000
+    case BTC: return 7; // 8,400,000 (# digits, without commas)
+    case mBTC: return 10; // 8,400,000,000
+    case uBTC: return 13; // 8,400,000,000,000
     default: return 0;
     }
 }
