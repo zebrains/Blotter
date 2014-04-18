13ccdb04077f6c428c73c0ee9da0e1852da5187f
Paul Bradley <paul@paul-bradley.com>
2014-04-18 12:30:14 +0100

    Fix about dialog copyright notices

--- src/qt/aboutdialog.cpp
+++ src/qt/aboutdialog.cpp
@@ -6,7 +6,7 @@
 
 // Copyright year (2009-this)
 // Todo: update this when changing our copyright comments in the source
-const int ABOUTDIALOG_COPYRIGHT_YEAR = 2013;
+const int ABOUTDIALOG_COPYRIGHT_YEAR = 2014;
 
 AboutDialog::AboutDialog(QWidget *parent) :
     QDialog(parent),
@@ -19,9 +19,9 @@ AboutDialog::AboutDialog(QWidget *parent) :
                QString(" &copy; 2009-%1 ").arg(COPYRIGHT_YEAR) +
                tr("The Bitcoin developers") + QString("<br>") +
                tr("Copyright") + QString(" &copy; ") +
-               tr("2011-%1 The Monocle developers").arg(ABOUTDIALOG_COPYRIGHT_YEAR) + QString("<br>") +
+               tr("2011-%1 The Litecoin developers").arg(ABOUTDIALOG_COPYRIGHT_YEAR) + QString("<br>") +
                tr("Copyright") + QString(" &copy; ") +
-               tr("2013 The Monocle developers"));
+               tr("%1 The Monocle developers").arg(ABOUTDIALOG_COPYRIGHT_YEAR));
 }
 
 void AboutDialog::setModel(ClientModel *model)
