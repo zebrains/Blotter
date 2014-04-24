98c6b613db41339457380463798bd82fcb9fdaa4
Erik Mossberg <lingonvecka@gmail.com>
2014-04-24 21:15:17 +0200

    updated max coins

--- src/main.h
+++ src/main.h
@@ -55,7 +55,7 @@ static const int64 DUST_SOFT_LIMIT = 100000; // 0.001 MON
 /** Dust Hard Limit, ignored as wallet inputs (mininput default) */
 static const int64 DUST_HARD_LIMIT = 1000;   // 0.00001 MON mininput
 /** No amount larger than this (in satoshi) is valid */
-static const int64 MAX_MONEY = 8400000 * COIN;
+static const int64 MAX_MONEY = 840000023785656;
 inline bool MoneyRange(int64 nValue) { return (nValue >= 0 && nValue <= MAX_MONEY); }
 /** Coinbase transaction outputs can only be spent after this number of new blocks (network rule) */
 static const int COINBASE_MATURITY = 100;
