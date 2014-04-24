583ccd4277759c86385f3c0b7c2afbdafe140d74
Paul Bradley <paul@paul-bradley.com>
2014-04-24 21:33:47 +0100

    Fees changes

--- src/wallet.cpp
+++ src/wallet.cpp
@@ -1321,7 +1321,8 @@ bool CWallet::CreateTransaction(const vector<pair<CScript, int64> >& vecSend,
 
                 // Check that enough fee is included
                 int64 nPayFee = nTransactionFee * (1 + (int64)nBytes / 1000);
-                bool fAllowFree = CTransaction::AllowFree(dPriority);
+            //    bool fAllowFree = CTransaction::AllowFree(dPriority); // No free TXs in MON 
+               bool fAllowFree = false;					// No free TXs in MON
                 int64 nMinFee = wtxNew.GetMinFee(1, fAllowFree, GMF_SEND);
                 if (nFeeRet < max(nPayFee, nMinFee))
                 {
