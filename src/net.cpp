61fd69d7ad49fc2a5ce04b7454d027f4ccdef0ac
Erik Mossberg <lingonvecka@gmail.com>
2014-05-17 21:20:22 +0200

    updated seed nodes

--- src/net.cpp
+++ src/net.cpp
@@ -1192,6 +1192,7 @@ void MapPort(bool)
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strMainNetDNSSeed[][2] = {
+    {"monseed.meningslos.info", "monseed.meningslos.info"},
     {"mon-seed.etyd.org", "mon-seed.etyd.org"},
     {"mon-seed2.etyd.org", "mon-seed2.etyd.org"},
     {"monexplorer.vertcoin.org", "monexplorer.vertcoin.org"},
